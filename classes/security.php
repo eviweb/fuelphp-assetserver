9936893adeb3315cd072a3aebe2c6886d8c688fc
Eric Villard <dev@eviweb.fr>
2012-10-12 19:15:39 +0200

    add security class

---
+++ classes/security.php
@@ -0,0 +1,59 @@
+<?php
+
+/* vim: set noexpandtab tabstop=8 shiftwidth=8 softtabstop=8: */
+/**
+ * The MIT License
+ *
+ * Copyright 2012 Eric VILLARD <dev@eviweb.fr>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * 
+ * @package     assetserver
+ * @author      Eric VILLARD <dev@eviweb.fr>
+ * @copyright	(c) 2012 Eric VILLARD <dev@eviweb.fr>
+ * @license     http://opensource.org/licenses/MIT MIT License
+ */
+
+namespace Assetserver;
+
+/**
+ * secure file serving
+ * 
+ * @package     assetserver
+ * @author      Eric VILLARD <dev@eviweb.fr>
+ * @copyright	(c) 2012 Eric VILLARD <dev@eviweb.fr>
+ * @license     http://opensource.org/licenses/MIT MIT License
+ */
+final class Security
+{	
+	/**
+	 * check if a file type is allowed, so the file can be served
+	 * 
+	 * @param string $type	file type
+	 * @returns boolean	returns true if the type is authorized, false if it is forbidden
+	 */
+	public static function is_allowed($type)
+	{
+		if (empty($type)) return false;
+		$types = \Config::get('assetserver.security.types_allowed');
+		$multi = preg_replace('/\/\w+/', '/*', $type);
+		return (isset($types[$type]) && $types[$type])
+			|| (isset($types[$multi]) && $types[$multi]);
+	}
+}
